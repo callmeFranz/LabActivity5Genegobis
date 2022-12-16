13663fa7ea257cda8e34b5a7b915a97fe24eb6f2
DELL <genegobis.franz@gmail.com>
2022-12-16 15:59:45 +0800

    Rename LeapYear components

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,8 +3,11 @@ package SimpleCalc;
 import javax.swing.*;
 
 public class SimpleCalcGUI {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
+    private JPanel jpanel;
+    private JTextField tfNumber1;
+    private JComboBox cbOperations ;
     private JButton computeResultButton;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
+
 }
