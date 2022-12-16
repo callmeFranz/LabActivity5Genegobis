e2744c8df0f537636b7620a55dfd2ae844ac066c
DELL <genegobis.franz@gmail.com>
2022-12-16 15:20:50 +0800

    Add LeapYear main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -2,8 +2,16 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
     private JPanel jpanel;
     private JTextField tfYear;
     private JButton btnCheckYear;
+
+    public static void main(String[] args) {
+        LeapYearGUI gui = new LeapYearGUI();
+        gui.setSize(550,600);
+        gui.setVisible(true);
+        gui.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        gui.setContentPane(gui.jpanel);
+    }
 }
