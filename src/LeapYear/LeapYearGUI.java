3791a1143286364820bf9951a2326e0fc2b42872
DELL <genegobis.franz@gmail.com>
2022-12-16 15:17:13 +0800

    Rename LeapYear components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -3,7 +3,7 @@ package LeapYear;
 import javax.swing.*;
 
 public class LeapYearGUI {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JPanel jpanel;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
 }
