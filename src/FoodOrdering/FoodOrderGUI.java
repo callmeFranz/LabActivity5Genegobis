8866147b0ca33f0957e02d51e42fcca104e1a8e6
DELL <genegobis.franz@gmail.com>
2022-12-16 19:16:52 +0800

    Rename FoodOrdering components

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -3,8 +3,17 @@ package FoodOrdering;
 import javax.swing.*;
 
 public class FoodOrderGUI {
-    private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JButton orderButton;
+    private JPanel jpanel;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
 }
