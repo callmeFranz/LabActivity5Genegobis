6e8a0ef33230c9fb9516656a3e2e04daa07f311e
DELL <genegobis.franz@gmail.com>
2022-12-16 19:21:35 +0800

    Add FoodOrdering main function

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,7 +2,7 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel jpanel;
     private JCheckBox cPizza;
     private JRadioButton rbNone;
@@ -16,4 +16,12 @@ public class FoodOrderGUI {
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    public static void main(String[] args) {
+        FoodOrderGUI gui = new FoodOrderGUI();
+        gui.setVisible(true);
+        gui.setSize(700, 500);
+        gui.setContentPane(gui.jpanel);
+        gui.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        gui.setTitle("Food Ordering System");
+    }
 }
